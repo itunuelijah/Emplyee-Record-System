3cad13454037bf5ce06ea1b97a65ed04b0a99fc2
itunuelijah <itunuelijah@gmail.com>
2022-02-16 09:20:06 +0100

    employee configuration

---
+++ src/main/java/com/employeeRecord/service/config/Configuration.java
@@ -0,0 +1,27 @@
+package com.employeeRecord.service.config;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+
+@org.springframework.context.annotation.Configuration
+public class Configuration {
+    @Value("${cloudinary.cloud-name}")
+    private String cloudName;
+    @Value("${cloudinary.api-key}")
+    private String apiKey;
+    @Value("${cloudinary.api-secret}")
+    private String apiSecret;
+
+//    note: ignore the file below from going to git
+    @Bean
+    public Cloudinary cloudinary(){
+        return new Cloudinary(ObjectUtils.asMap(
+                        "cloud_name", cloudName,
+                        "api_key", apiKey,
+                        "api_secret", apiSecret,
+                        "secure", true));
+    }
+}
