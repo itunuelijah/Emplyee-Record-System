2521063428266766053ebbf1f767b543295ea53f
itunuelijah <itunuelijah@gmail.com>
2022-02-16 09:24:38 +0100

    sql

--- src/main/resources/db/insert.sql
+++ src/main/resources/db/insert.sql
@@ -1,6 +1,6 @@
 truncate table employee;
 
-insert into employee(id, name, gender, age)
-values(12, 'Luxury Mop', 'male', 3),
-      (13, 'Macbook Air', 'female', 4),
-      (14, 'Rocking chair', 'male', 5);
+insert into employee(id, firt_name, last_name, gender, age)
+values(12, 'Luxury', ' Mop', 'male', 3),
+      (13, 'Macbook', 'Air', 'female', 4),
+      (14, 'Rocking',  'Chair', 'male', 5);
