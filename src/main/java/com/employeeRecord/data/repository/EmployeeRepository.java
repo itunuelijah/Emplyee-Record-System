464cd20b103b7346db91dea2add32ae239669c39
itunuelijah <itunuelijah@gmail.com>
2022-02-16 09:18:25 +0100

    employee issue solved

--- src/main/java/com/employeeRecord/data/repository/EmployeeRepository.java
+++ src/main/java/com/employeeRecord/data/repository/EmployeeRepository.java
@@ -1,6 +1,6 @@
-package data.repository;
+package com.employeeRecord.data.repository;
 
-import data.models.Employee;
+import com.employeeRecord.data.models.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +10,7 @@ import java.util.Optional;
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     Optional<Employee> findByFirstName (String firstName);
+
+    Optional<Employee> findEmployeeById(Long employeeId);
 
 }
