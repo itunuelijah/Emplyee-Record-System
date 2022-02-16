29eace9afaa44cc39064f37758738aedb35aed6b
itunuelijah <itunuelijah@gmail.com>
2022-02-16 09:23:58 +0100

    exception issue solved

--- src/main/java/com/employeeRecord/web/exceptions/EmployeeDoesNotExistException.java
+++ src/main/java/com/employeeRecord/web/exceptions/EmployeeDoesNotExistException.java
@@ -1,8 +1,9 @@
-package web.exceptions;
+package com.employeeRecord.web.exceptions;
 
 public  class EmployeeDoesNotExistException extends Throwable{
 
     public EmployeeDoesNotExistException(String message){
+
         super(message);
     }
 
